42c815a811b0e517c26cf60b41d46f89b64464a6
adelavegaf <antoniodlavga@gmail.com>
2017-08-29 12:00:53 -0500

    Fix variable name used in inner loop

--- run-fibonacci.sh
+++ run-fibonacci.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-
+# Please make sure you have ran setup.sh before executing this script. Otherwise it will fail!
 NUM_PROCESSES="$1"
 NICE_LEVEL="$2"
 
-for ((i=0; i<NUM_FOREGROUND_PROCESSES; i=i+1))
+for ((i=0; i<NUM_PROCESSES; i=i+1))
 do
     nice -n "$NICE_LEVEL" './unacloud-test-files/bin/fibonacci' &
 done
