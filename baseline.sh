c1d01cc84f95733ce81a74ccca9dca3f64c6444e
adelavegaf <antoniodlavga@gmail.com>
2017-09-02 20:12:19 -0500

    Add loop to try all possible combinations between foreground and background tasks.

--- baseline.sh
+++ baseline.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
-NUM_FOREGROUND_PROCESSES="$1"
-NUM_BACKGROUND_PROCESSES="$2"
-
-(time ./run-fibonacci.sh "$NUM_FOREGROUND_PROCESSES" 0) > "foreground_${NUM_FOREGROUND_PROCESSES}_${NUM_BACKGROUND_PROCESSES}.txt" 2>&1 &
-(time ./run-fibonacci.sh "$NUM_BACKGROUND_PROCESSES" 19) > "background_${NUM_FOREGROUND_PROCESSES}_${NUM_BACKGROUND_PROCESSES}.txt" 2>&1 &
-wait
+NUM_CORES="$(grep -c ^processor /proc/cpuinfo)"
+for ((i=1;i<NUM_CORES; i=i+1))
+do
+    for ((j=0; j<NUM_CORES; j=j+1))
+    do
+        (time ./run-fibonacci.sh "$i" 0) > "foreground_${i}_${j}.txt" 2>&1 &
+        (time ./run-fibonacci.sh "$j" 19) > "background_${i}_${j}.txt" 2>&1 &
+        wait
+        sleep 1m
+    done
+done
 
