3d24e5cdc4964d56e61bf1a1056718880508b515
adelavegaf <antoniodlavga@gmail.com>
2017-09-03 19:20:56 -0500

    Bug fix: loops now take all available cores.

--- baseline.sh
+++ baseline.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 NUM_CORES="$(grep -c ^processor /proc/cpuinfo)"
-for ((i=1;i<NUM_CORES; i=i+1))
+for ((i=1;i<=NUM_CORES; i=i+1))
 do
-    for ((j=0; j<NUM_CORES; j=j+1))
+    for ((j=0; j<=NUM_CORES; j=j+1))
     do
         (time ./run-fibonacci.sh "$i" 0) > "foreground_${i}_${j}.txt" 2>&1 &
         (time ./run-fibonacci.sh "$j" 19) > "background_${i}_${j}.txt" 2>&1 &
