09c0990b89d2e8244bde1be5e98806dcf8c07dfb
adelavegaf <antoniodlavga@gmail.com>
2017-11-18 11:48:00 -0500

    Fix cpulimit usage.

--- run-fibonacci-cpulimit.sh
+++ run-fibonacci-cpulimit.sh
@@ -6,7 +6,8 @@ PERCENTAGE="$2"
 
 for ((i=0; i<NUM_PROCESSES; i=i+1))
 do
-    cpulimit -l "$PERCENTAGE" -e ./unacloud-test-files/bin/fibonacci 200000000 80 23416728348467685 &
+    ./unacloud-test-files/bin/fibonacci 200000000 80 23416728348467685 &
+    cpulimit -l "$PERCENTAGE" -p "$!"
 done
 
 wait
