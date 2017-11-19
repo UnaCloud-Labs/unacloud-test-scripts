5475193ceeddb6b023acbf06c13b421c80921590
Antonio de la Vega <antoniodlavag@gmail.com>
2017-11-19 17:30:21 -0500

    Fix cpu limit bug

--- run-fibonacci-cpulimit.sh
+++ run-fibonacci-cpulimit.sh
@@ -7,7 +7,7 @@ PERCENTAGE="$2"
 for ((i=0; i<NUM_PROCESSES; i=i+1))
 do
     ./unacloud-test-files/bin/fibonacci 200000000 80 23416728348467685 &
-    cpulimit -l "$PERCENTAGE" -p "$!"
+    cpulimit -l "$PERCENTAGE" -p "$!" &
 done
 
 wait
